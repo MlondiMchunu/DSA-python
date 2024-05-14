5bfc36e5fe630d09523847375e3c74b83accf09e
Stopa <sikhuliso@gmail.com>
2024-05-14 12:44:01 +0200

    commit insert

--- LinkedList/linkedList.py
+++ LinkedList/linkedList.py
@@ -32,6 +32,56 @@ class LinkedList:
             itr = itr.next
         itr.next = Node(data,None)
 
+    def insert_values(self,data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_the_end(data)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
+    
+    def remove_at(self,index):
+        if index<0 or index>=self.get_length():
+            raise Exception("Invalid Index")
+        if index ==0:
+            self.head = self.head.next
+            return
+        
+        count =0
+        itr = self.head
+
+        while itr:
+            if count == index-1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+
+    def insert_at(self,index,data):
+        if index<0 or index>=self.get_length:
+            raise Exception("Invalid Index")
+        
+        if index == 0:
+            self.insert_at_beginning(data)
+            return
+        
+        count = 0
+
+        itr = self.head
+
+        while itr:
+            if count == index-1:
+                node = Node(data,itr.next)
+                itr.next = Node
+                break
+            itr = itr.next
+            count +=1
+
 
 if __name__ == '__main__':
     ll = LinkedList()
@@ -40,4 +90,8 @@ if __name__ == '__main__':
     ll.insert_at_beginning(7)
 
     ll.insert_at_the_end(74)
+
+    ll.insert_values(['mlo','jeff','said','gone','late'])
+    ll.remove_at(3)
     ll.print()
+    print("Length of LinkedList : ",ll.get_length())
