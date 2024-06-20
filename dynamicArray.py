96d103d80873dbb8618d5b54996487243f25d553
Stopa <sikhuliso@gmail.com>
2024-06-20 21:15:41 +0200

    dynamic array

---
+++ dynamicArray.py
@@ -0,0 +1,35 @@
+class DynamicArray:
+    def __init__(self,capacity:int):
+        self.size = 0
+        self.capacity = capacity
+        self.array = [0]* capacity
+
+
+    def insert(self, i:int, n:int)->None:
+        self.arr[i] = n
+
+    def get(self,index:int)->int:
+        return self.arr[index]
+    
+    def pushback(self, element:int)->None:
+        if self.size == self.capacity:
+            self.resize()
+
+        self.arr[self.size] = element
+        self.size+=1
+
+    def resize(self)->None:
+        self.capacity = 2 * self.capacity
+        new_arr = [0] * self.capacity
+
+        for i in range(self.size):
+            new_arr[i] = self.arr[i]
+            self.arr = new_arr
+
+    
+
+    def getSize(self):
+        return self.size
+    
+    def getCapacity(self):
+        return self.capacity
